fa2201639827aee13d58365dcf8f05e6f2109087
aq <alqar003@cougars.csusm.edu>
2021-05-15 12:03:05 -0700

    FirstCommit

--- app/src/main/java/com/example/finalproject/GoalSetupActivity.kt
+++ app/src/main/java/com/example/finalproject/GoalSetupActivity.kt
@@ -1,5 +1,6 @@
 package com.example.finalproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,10 @@ class GoalSetupActivity :AppCompatActivity() {
         setContentView(R.layout.goalsetup_layout)
 
         val gButton = findViewById<Button>(R.id.goalSubmitButton)
+        val goalSetup = findViewById<Button>(R.id.goalCancelButton)
+
+
+
 
 
     }
--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        addButton.setOnClickListener{
+            val intent= Intent(this, GoalSetupActivity::class.java )
+            startActivity(intent);
+        }
+
     }
 }
